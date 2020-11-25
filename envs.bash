499aa8f62fd94240c3b9313217937dfc0b4912ff
Shintaro Iwasaki <siwasaki@anl.gov>
2020-11-25 16:17:11 +0000

    update README.md

--- envs.bash
+++ envs.bash
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-ROOT_DIR=$(cd $(dirname $0) && pwd -P)
-cd $ROOT_DIR
+# location of Intel compiler package
+export INTEL_DIR=/soft/compilers/intel-2019/compilers_and_libraries_2019.4.243/linux
 
+# machine config: number of cores per node
+export N_CORES=56
+
+# machine config: number of sockets per node
+export N_SOCKETS=2
+
+# root directory, which must point to the top directory of the artifact
+ROOT_DIR=$(cd $(dirname -- $0) && pwd -P)
+
+# install directory (default: ROOT_DIR/opt)
 INSTALL_DIR=${ROOT_DIR}/opt
 
-# load intel compilers
-export INTEL_DIR=/soft/compilers/intel-2019/compilers_and_libraries_2019.4.243/linux
-source ${INTEL_DIR}/bin/compilervars.sh intel64
+################################################################
 
-# machine config
-export N_CORES=56
-export N_SOCKETS=2
+# check root directory
+if test ! -d $ROOT_DIR/argobots ; then
+  echo "Error: incorrect root directory. Check ROOT_DIR=${ROOT_DIR}"
+  exit 1
+fi
+
+# move to ROOT_DIR
+cd $ROOT_DIR
+
+# load intel compilers
+source ${INTEL_DIR}/bin/compilervars.sh intel64
+if test x"$(which icc 2>/dev/null)" = 'x' ; then
+  echo "Error: icc is not found.  Check INTEL_DIR=${INTEL_DIR}"
+  exit 1
+fi
