5355e28a99e190fdeb31e2ad87b75419dbc9c8c5
Shumpei Shiina <shiina@eidos.ic.i.u-tokyo.ac.jp>
2020-11-26 21:07:40 +0900

    fix compiler flags

--- preemption_benchmarks/run_jobs/preemption_cost2.bash
+++ preemption_benchmarks/run_jobs/preemption_cost2.bash
@@ -4,19 +4,19 @@ export LC_ALL=C
 export LANG=C
 
 make clean
-CC=icc CFLAGS="-DUSE_PTHREAD=1" make preemption_cost2
+CFLAGS="-DUSE_PTHREAD=1" make preemption_cost2
 echo ""
 echo "## Pthread"
 taskset -c 0 ./preemption_cost2 -x 1 -g 1 -r 1 -p 1000
 
 make clean
-CC=icc CFLAGS="-DPREEMPTION_TYPE=ABT_PREEMPTION_YIELD" make preemption_cost2
+CFLAGS="-DPREEMPTION_TYPE=ABT_PREEMPTION_YIELD" make preemption_cost2
 echo ""
 echo "## Argobots (signal-yield)"
 taskset -c 0 ./preemption_cost2 -x 1 -g 1 -r 1 -p 1000 -t 10000
 
 make clean
-CCC=icc FLAGS="-DPREEMPTION_TYPE=ABT_PREEMPTION_NEW_ES" make preemption_cost2
+CFLAGS="-DPREEMPTION_TYPE=ABT_PREEMPTION_NEW_ES" make preemption_cost2
 echo ""
 echo "## Argobots (KLT-switching)"
 taskset -c 0 ./preemption_cost2 -x 1 -g 1 -r 1 -p 1000 -t 10000
